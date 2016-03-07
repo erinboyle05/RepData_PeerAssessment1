5efa37682a915defc8d6ff2cdfeb53ca1447d41a
erinboyle05 <erinboyle05@gmail.com>
2016-03-07 15:25:00 -0500

    Start assignment

---
+++ getdata.R
@@ -0,0 +1,11 @@
+# This file downloads the data from the web, unzips the file
+# and loads the data
+
+if (!file.exists("activity.csv")) {
+        
+        file_url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
+        download.file(file_url, "activity.zip", mode = "wb")
+        unzip("activity.zip")
+}
+data <- read.csv("activity.csv")
+                   
